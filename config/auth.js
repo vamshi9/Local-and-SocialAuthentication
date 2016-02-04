ab7169330432b73f0f71a27cdf60a6725d5e6bfa
vamshi <vamsisai999@gmail.com>
2016-02-04 12:55:07 +0530

    auth

--- config/auth.js
+++ config/auth.js
@@ -1,20 +1,20 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'      : '780592965379851', // your App ID
-        'clientSecret'  : 'd415766d7eb80c34ad0c77cd9a44a4a5', // your App Secret
+        'clientID'      : '#', // your App ID
+        'clientSecret'  : '#', // your App Secret
         'callbackURL'   : 'http://localhost:2626/auth/facebook/callback'
     },
 
     'twitterAuth' : {
-        'consumerKey'       : 'your-consumer-key-here',
-        'consumerSecret'    : 'your-client-secret-here',
+        'consumerKey'       : '#',
+        'consumerSecret'    : '#',
         'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
     },
 
     'googleAuth' : {
-        'clientID'      : '158590619508-kb6a7dv7ooo0nmsjq8iqoupmtavfhqf4.apps.googleusercontent.com',
-        'clientSecret'  : 'UbYHyTNZUXxfe62UhnmRv_Ad',
+        'clientID'      : '#',
+        'clientSecret'  : '#',
         'callbackURL'   : 'http://localhost:2626/auth/google/callback'
     }
 
